31640c2a48b26551ddfd8e3ca505645a251ff0b7
Eduardo Souza <eduardofidera@gmail.com>
2018-04-07 19:29:43 -0300

    init module

---
+++ Fidera/Component/Block/Index/Index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+namespace Fidera\Component\Block\Index;
+
+class Index extends \Magento\Framework\View\Element\Template
+{
+
+}
---
+++ Fidera/Component/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+"use strict";
+var config = {
+    map: {
+        '*': {
+            'reactComponent': 'Fidera_Component/js/bundle'
+        }
+    }
+};
