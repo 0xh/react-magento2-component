17dcfe8107875ce8b85064642b398b0d72e04cf1
Eduardo Souza <eduardofidera@gmail.com>
2018-04-08 01:53:12 -0300

    added some react sample code

--- Fidera/Component/view/frontend/src/app.js
+++ Fidera/Component/view/frontend/src/app.js
@@ -1,1 +1,10 @@
-console.log('ready');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Component = () => (
+    <div>
+        <h1>This is the React Component under my Magento 2 module</h1>
+    </div>
+);
+
+ReactDOM.render(<Component />, document.getElementById('app'));
